9701318cc0f96aa9447c0195fac0954af2679030
swkenz <supakorn02@gmail.com>
2025-03-16 02:09:57 +0700

    loginhome

--- Store-Service/src/app/app.module.ts
+++ Store-Service/src/app/app.module.ts
@@ -1,28 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-// Import PrimeNG modules
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { StepsModule } from 'primeng/steps';
 import { CalendarModule } from 'primeng/calendar';
 import { StepperModule } from 'primeng/stepper';
 import { ToastModule } from 'primeng/toast';
+import { ImageModule } from 'primeng/image';
+import { MenubarModule } from 'primeng/menubar';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
+
 @NgModule({
-  declarations: [],
+  declarations: [], // ✅ ถ้าไม่มี Component หรือ Directive ไม่จำเป็นต้องมี
   imports: [
     CommonModule,
+    FormsModule,
     ButtonModule,
     CalendarModule,
     StepperModule,
     StepsModule,
     ToastModule,
+    ImageModule,
+    VirtualScrollerModule,
+    MenubarModule
   ],
   exports: [
+    FormsModule, // ✅ Export FormsModule ออกไป
     ButtonModule,
     CalendarModule,
     StepperModule,
     StepsModule,
     ToastModule,
+    ImageModule,
+    MenubarModule,
+    VirtualScrollerModule
   ]
 })
 export class PrimeNgModule {}
